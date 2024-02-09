be3b6fec0d6841f7b729be8bfdd634cb069e530d
PStewart1 <4920@holbertonstudents.com>
2024-02-09 13:43:09 -0600

    added documentation

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,9 +1,16 @@
+/// accepts a list of doubles representing a list of temperatures in celsius, converts them to fahrenheit and returns them in a list.
 List<double> convertToF(List<double> temperaturesInC) {
+
+  // List of temperatures in Farenheit, as doubles rounded to 2 decimal places
   List<double> temperaturesInF = [];
+  // loop through list
   for (double i in temperaturesInC) {
-    i = (i * 9/5) + 32;
-    String num = i.toStringAsFixed(2);
-    temperaturesInF.add(double.parse(num));
+    // calculate farenheit temp
+    i = (i * 9 / 5) + 32;
+    // temperature rounded, as a string.
+    String temp = i.toStringAsFixed(2);
+    // convert back to double, and add to list
+    temperaturesInF.add(double.parse(temp));
   }
-  return  temperaturesInF;
+  return temperaturesInF;
 }
