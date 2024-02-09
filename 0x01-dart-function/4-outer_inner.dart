ddd7413a7d9416f0f3375ae34d192d13607e82cd
PStewart1 <4920@holbertonstudents.com>
2024-02-09 12:26:25 -0600

    4. Outer Inner

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,10 @@
+void outer(String name, String id) {
+  String inner() {
+    List nameList = name.split(' ');
+    String initial = nameList[1].substring(0,1);
+    String firstName = nameList[0];
+    return 'Hello $initial.$firstName your id is $id';
+  }
+
+  print(inner());
+}
