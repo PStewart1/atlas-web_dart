e21778eeec13316255b27ca4948ffc25e17ba362
PStewart1 <4920@holbertonstudents.com>
2024-02-09 12:44:08 -0600

    added 'Agent'

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -3,7 +3,7 @@ void outer(String name, String id) {
     List nameList = name.split(' ');
     String initial = nameList[1].substring(0,1);
     String firstName = nameList[0];
-    return 'Hello $initial.$firstName your id is $id';
+    return 'Hello Agent $initial.$firstName your id is $id';
   }
 
   print(inner());
--- 0x01-dart-function/main.dart
+++ 0x01-dart-function/main.dart
@@ -1,7 +1,5 @@
-import '5-calculate-area.dart';
+import '4-outer_inner.dart';
 
-main() {
-  print(calculateArea(20,7));
-  print(calculateArea(10,7));
-  print(calculateArea(8.5,3.7));
+void main() {
+  outer("Youssef Belhadj", "001");
 }
