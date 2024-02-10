0565b8a01eec2e1bda4fd87e4a90e7b5c2164f83
PStewart1 <4920@holbertonstudents.com>
2024-02-10 17:15:36 -0600

    5. Json to User

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,22 @@
+/// Basic user class. contains [name], [age], [height], and [toJson].
+class User {
+  User({required this.name, required this.age, required this.height, required this.id});
+  /// [User]'s name.
+  String name;
+  /// [User]'s age 
+  int age;
+  /// [User]'s height
+  double height;
+  /// [User]'s id
+  int id;
+  /// Returns a map representation of the User
+  Map toJson() => { 'id': id, 'name': name, 'age': age, 'height': height };
+  /// Creates a User instane from a json map object
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(name: userJson['name'], age: userJson['age'], height: userJson['height'], id: userJson['id']);
+  }
+  /// a custom toString message
+  @override
+  String toString() => 'User(id: $id ,name: $name, age: $age, height: $height';
+}
+
