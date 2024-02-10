16d9607963a46487688d8433d4ac4a7dacd6ed16
PStewart1 <4920@holbertonstudents.com>
2024-02-10 15:09:54 -0600

    0. Welcome.

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,7 @@
+/// Basic user class. contains a late string [name], and [showName].
+class User {
+  /// A name.
+  late String name;
+  /// Returns "Hello [name]"
+  String showName() => 'Hello $name';
+}
