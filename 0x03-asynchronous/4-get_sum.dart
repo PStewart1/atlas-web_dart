219350bf22b1fc4f40322ad9a2c571f7d67be91c
PStewart1 <4920@holbertonstudents.com>
2024-02-10 21:48:52 -0600

    4. Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,32 @@
+import '4-util.dart';
+import 'dart:convert';
+/// Returns the total price of the user's order.
+/// Calls [fetchUserData], [fetchUserOrders], and [fetchProductPrice].
+Future<double> calculateTotal() async {
+  try {
+    // fetchUserData returns a string in json format, but we only want the id
+    String str = await fetchUserData();
+    // so we must convert it to a map, in order to easily call it's individual values.
+    Map<String, dynamic> userMap = jsonDecode(str);
+    // then we can easily extract the id
+    String id = userMap['id'];
+    // and use it to retrieve the user's orders
+    String orders = await fetchUserOrders(id);
+    // which returns a list, in string form, so again we must convert it to a list of strings.
+    List<String> orderList = json.decode(orders).cast<String>();
+    // then we prepare our total price tally
+    double total = 0;
+    // a forEach loop wont work as it isn't async, but a for-in loop can be.
+    for (var item in orderList) {
+      // now we can retrieve the price for each order, 
+      String priceStr = await fetchProductPrice(item);
+      // and add it to our total.
+      total += double.parse(priceStr);
+    };
+    // Done and done.
+    return total; 
+  } catch (e) {
+    // return -1 for error.
+    return -1;
+  }
+}
